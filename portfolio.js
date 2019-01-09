7ce0b092fc0eed9ad69cbd7c46483aed389a55f7
Ryotaman213 <ryotafreeman@gmail.com>
2019-01-09 20:00:47 +0900

    addedjs

---
+++ portfolio.js
@@ -0,0 +1,19 @@
+function showElementAnimation() {
+
+  var element = document.getElementsByClassName('js-animation');
+  if(!element) return; // 要素がなかったら処理をキャンセル
+
+  var showTiming = window.innerHeight > 768 ? 500 : 70;
+  var scrollY = window.pageYOffset;
+  var windowH = window.innerHeight;
+
+  for(var i=0;i<element.length;i++) { var elemClientRect = element[i].getBoundingClientRect(); var elemY = scrollY + elemClientRect.top; if(scrollY + windowH - showTiming > elemY) {
+      element[i].classList.add('is-show');
+    } else if(scrollY + windowH < elemY) {
+
+      element[i].classList.remove('is-show');
+    }
+  }
+}
+showElementAnimation();
+window.addEventListener('scroll', showElementAnimation);
